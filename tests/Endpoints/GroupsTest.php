4e385dd2b63a12d238cd41d82a98e681ae4c0049
Malcolm Christie <malcolm@christiesmedia.co.uk>
2020-03-28 13:02:27 +0000

    Test with a MySQL database?

--- tests/Endpoints/GroupsTest.php
+++ tests/Endpoints/GroupsTest.php
@@ -9,7 +9,7 @@ class GroupsTest extends TestCase
 {
     public function test_get_groups()
     {
-        $response = $this->get('/groups');
+        $response = $this->get('/api/groups');
 
         $response->assertStatus(200);
         $response->assertJson([]);
@@ -17,11 +17,11 @@ class GroupsTest extends TestCase
 
     public function test_get_group_by_id()
     {
-        $response = $this->get('/groups/bd2b3e2f-74a9-4eb6-9a70-853af14a7510');
+        $response = $this->get('/api/groups/9ae30e83-4e5a-4eca-a74c-4f7663c84cea');
 
         $response->assertStatus(200);
         $response->assertJson([
-            'id' => 'bd2b3e2f-74a9-4eb6-9a70-853af14a7510',
+            'id' => '9ae30e83-4e5a-4eca-a74c-4f7663c84cea',
             //
         ]);
     }
