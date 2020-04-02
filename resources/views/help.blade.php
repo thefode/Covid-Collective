5bde9e7a781f00a70c56c12619bd498deba752e1
Malcolm Christie <malcolm@christiesmedia.co.uk>
2020-04-02 22:36:07 +0100

    Catriona doesn't like commas, or links opening in the same tab.

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -10,25 +10,25 @@
         <h4 class="mt-4">1. Follow the guidance</h4>
         <p>The most important thing to do is follow the governments guidance on how to reduce the spread and flatten the curve.</p>
         <p>
-            <a href="https://www.gov.uk/coronavirus">Read the GovUK Guidance &raquo;</a>
+            <a target="_blank" href="https://www.gov.uk/coronavirus">Read the Gov.UK Guidance &raquo;</a>
         </p>
 
         <h4 class="mt-4">2. Track your symptoms</h4>
         <p>Report your symptom (or lack of them) dailing using COVID Sympton Tracker app to help track the spread of the coronavirus.</p>
         <p>
-            <a href="https://covid.joinzoe.com/">Get the app at covid.joinzoe.com &raquo;</a>
+            <a target="_blank" href="https://covid.joinzoe.com/">Get the app at covid.joinzoe.com &raquo;</a>
         </p>
         
         <h4 class="mt-4">3. Join a local support group</h4>
-        <p>There are support groups all over the country, find and join yours today.</p>
+        <p>There are support groups all over the country. Find and join yours today.</p>
         <p>
-            <a href="{{route('groups')}}">Find a support group &raquo;</a>
+            <a target="_blank" href="{{route('groups')}}">Find a support group &raquo;</a>
         </p>
         
         <h4 class="mt-4">4. Lend us a hand</h4>
         <p>This is a one-man project currently, but you can contibute through GitHub.</p>
         <p>
-            <a href="https://github.com/mchristie/Covid-Collective">Fork me on GitHub &raquo;</a>
+            <a target="_blank" href="https://github.com/mchristie/Covid-Collective">Fork me on GitHub &raquo;</a>
         </p>
 
     </div>
